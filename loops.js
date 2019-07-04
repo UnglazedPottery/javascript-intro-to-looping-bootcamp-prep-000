cb03a2fc0d46fe1c05cb93b8abc3d6be999ac497
Jade Huynh <35jadeh@gmail.com>
2019-07-04 21:39:06 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,8 +1,8 @@
 function forLoop(array)
 {
   for (var i = 0; i < 25; i++) {
-    ifarray.push("I am 1 strange loop.");
-}
+    if array.push("I am 1 strange loop.");
+  }
 }
 
 function whileLoop(n)
